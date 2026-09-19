2d47a229040fe4a199630e8eb00a09abbb9adf7a
智商已更新 <3304711297@qq.com>
2026-09-19 14:19:58 +0800

    fix(tauri): suppress msvc linker stdout warning via linker_messages lint

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,7 @@
 // Tauri v2 入口
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![allow(unknown_lints)]
+#![allow(linker_messages)]
 
 fn main() {
     workbuddy2api::run_app();
